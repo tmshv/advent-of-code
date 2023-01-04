5374468c6deef8fd6e3ffc530fad808eefc03b60
Roman Timashev <roman@tmshv.ru>
2023-01-04 20:24:43 +0600

    rock location tl -> bl

--- 2022/day_17/src/bin/two.rs
+++ 2022/day_17/src/bin/two.rs
@@ -2,6 +2,7 @@ use std::{fmt::Debug, io, vec};
 
 use moveslice::Moveslice;
 
+#[derive(Debug)]
 enum Jet {
     Left,
     Right,
@@ -19,19 +20,29 @@ enum Jet {
 #[derive(Debug)]
 struct Grid {
     grid: [u8; 60],
-    high_index: isize,
+    high_index: usize,
     shift: u64,
 }
 
 impl Grid {
     fn new(grid: Option<[u8; 60]>) -> Grid {
         let grid = match grid {
-            None => [0; 60],
+            None => [
+                0b1111111, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
+                0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
+                0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
+                0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
+                0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
+                0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
+                0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
+                0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
+                0b0000000, 0b0000000, 0b0000000, 0b0000000,
+            ],
             Some(grid) => grid,
         };
         Grid {
             grid,
-            high_index: -1,
+            high_index: 0,
             shift: 0,
         }
     }
@@ -47,22 +58,25 @@ impl Grid {
         self.grid[50..].fill(0);
 
         // decreate top
+        // if self.high_index >= 10 {
         self.high_index -= 10;
+        // }
     }
 
     fn draw_shape(&mut self, shape: &Shape) {
         for (y, row) in shape.iter_rows() {
             self.grid[y] = row | self.grid[y];
         }
-        if shape.location.1 > self.high_index {
-            self.high_index = shape.location.1;
+        let h = shape.height - 1;
+        let top = shape.location.1 + h;
+        if top > self.high_index {
+            self.high_index = top;
         }
     }
 
     fn contains(&self, shape: &Shape) -> bool {
         for (y, row) in shape.iter_rows() {
-            let intersect = row & self.grid[y] > 0;
-            if intersect {
+            if row & self.grid[y] > 0 {
                 return true;
             }
         }
@@ -72,16 +86,16 @@ impl Grid {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct Shape {
-    location: (isize, isize),
+    location: (usize, usize),
     data: [u8; 4],
-    width: isize,
-    height: isize,
-    right_border: isize,
+    width: usize,
+    height: usize,
+    right_border: usize,
 }
 
 impl Shape {
-    fn new(width: isize, data: [u8; 4]) -> Shape {
-        let height = data.iter().filter(|row| **row != 0).count() as isize;
+    fn new(width: usize, data: [u8; 4]) -> Shape {
+        let height = data.iter().filter(|row| **row != 0).count();
 
         Shape {
             location: (0, 0),
@@ -92,11 +106,11 @@ impl Shape {
         }
     }
 
-    fn set_right_border(&mut self, border: isize) {
+    fn set_right_border(&mut self, border: usize) {
         self.right_border = border - self.width;
     }
 
-    fn set_location(&mut self, x: isize, y: isize) {
+    fn set_location(&mut self, x: usize, y: usize) {
         self.location = (x, y);
     }
 
@@ -112,29 +126,31 @@ impl Shape {
         self.location.1 += 1;
     }
 
-    fn move_left(&mut self) {
+    fn move_left(&mut self) -> bool {
         if self.location.0 > 0 {
             self.location.0 -= 1;
+            return true;
         }
+        false
     }
 
-    fn move_right(&mut self) {
+    fn move_right(&mut self) -> bool {
         if self.location.0 < self.right_border {
             self.location.0 += 1;
+            return true;
         }
+        false
     }
 
     fn iter_rows(&self) -> impl Iterator<Item = (usize, u8)> + '_ {
-        self.data
+        self.data[0..self.height]
             .iter()
             .enumerate()
-            .map(|(i, row)| (self.location.1 - i as isize, row >> self.location.0))
-            .filter(|(i, _)| *i >= 0)
-            .map(|(y, row)| (y as usize, row))
+            .map(|(i, row)| (self.location.1 + i, row >> self.location.0))
     }
 }
 
-fn get_shapes(right_border: isize) -> Vec<Shape> {
+fn get_shapes(right_border: usize) -> Vec<Shape> {
     let mut result = vec![
         // ####
         Shape::new(
@@ -164,10 +180,14 @@ fn get_shapes(right_border: isize) -> Vec<Shape> {
         Shape::new(
             3,
             [
+                0b1110000, // ..#
                 0b0010000, // ..#
-                0b0010000, // ..#
-                0b1110000, // ###
+                0b0010000, // ###
                 0b0000000, // unused
+                // 0b0010000, // ..#
+                // 0b0010000, // ..#
+                // 0b1110000, // ###
+                // 0b0000000, // unused
             ],
         ),
         // #
@@ -236,37 +256,52 @@ fn solve(jets: Vec<Jet>, rocks: u64) -> Grid {
 
         // 2. fill grid with empty rows
         // take the height of rock + 3 rows for the bottom or last pixel
-        let position = grid.high_index + rock.height + 3; // 2 means 3 pixels higher
+        let position = grid.high_index + 1 + 3;
         rock.set_location(2, position);
+
+        println!("rock {} will start from {:?}", i, rock.location);
 
         // 3. drop it with jet stream until it will be at the bottom
         loop {
             let jet = jet_cycle.next().unwrap();
             match jet {
                 Jet::Left => {
-                    rock.move_left();
+                    if rock.move_left() {
+                        println!("move {:?}", jet);
+                    } else {
+                        // rock.move_right();
+                        println!("move {:?} but nothing has happened", jet);
+                    }
                     if grid.contains(&rock) {
                         rock.move_right();
                     }
                 }
                 Jet::Right => {
-                    rock.move_right();
+                    if rock.move_right() {
+                        println!("move {:?}", jet);
+                    } else {
+                        // rock.move_left();
+                        println!("move {:?} but nothing has happened", jet);
+                    }
                     if grid.contains(&rock) {
                         rock.move_left();
                     }
                 }
             }
 
-            let failing = rock.move_down();
-            if !failing || grid.contains(&rock) {
-                if failing {
-                    rock.move_up();
-                }
+            rock.move_down();
+            if grid.contains(&rock) {
+                println!("move down causing it to ground");
+                rock.move_up();
 
                 grid.draw_shape(&rock);
                 break;
+            } else {
+                println!("move down");
             }
         }
+
+        println!("top: {}", grid.high_index);
 
         if grid.high_index > 50 {
             grid.shift();
@@ -276,23 +311,31 @@ fn solve(jets: Vec<Jet>, rocks: u64) -> Grid {
 }
 
 fn part_two(grid: &Grid) -> u64 {
-    grid.shift + grid.high_index as u64 + 1 // 1 cause index starts from 0
+    grid.shift + grid.high_index as u64
 }
 
 fn main() {
     // let rocks = 2022;
-    let rocks = 1_000_000;
+    // let rocks = 1_000_000;
     // let rocks = 1_000_000_000;
     // let rocks = 1_000_000_000_000;
+    let rocks = 10;
 
     let jets = read_input();
     let grid = solve(jets, rocks);
     let top = part_two(&grid);
 
     // println!("Result: {} ({})", top, top == 3153);
-    println!("Result: {} ({})", top, top == 1553686);
+    // println!("Result: {} ({})", top, top == 1553686);
     // println!("Result: {} ({})", top, top == 1553665705);
-    // println!("Result: {}", top);
+    println!("Result: {}", top);
+
+    for i in grid.grid.iter().rev() {
+        if *i == 0 {
+            continue;
+        }
+        println!("{:#09b}", i);
+    }
 }
 
 #[cfg(test)]
@@ -310,6 +353,7 @@ mod tests {
                 _ => panic!("Wrong char"),
             })
             .collect::<Vec<Jet>>();
+        println!("{:?}", jets);
         let grid = solve(jets, 1);
         let top = part_two(&grid);
         assert_eq!(top, 1);
@@ -329,7 +373,6 @@ mod tests {
         let grid = solve(jets, 2);
         let top = part_two(&grid);
         assert_eq!(top, 4);
-
         let mut result: [u8; 60] = [
             0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
             0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
@@ -351,11 +394,11 @@ mod tests {
             0b0000000, // 07
             0b0000000, // 06
             0b0000000, // 05
-            0b0000000, // 04
-            0b0001000, // 03
-            0b0011100, // 02
-            0b0001000, // 01
-            0b0011110, // 00
+            0b0001000, // 04
+            0b0011100, // 03
+            0b0001000, // 02
+            0b0011110, // 01
+            0b1111111, // 00
         ];
         result.reverse();
         assert_eq!(grid.grid, result);
@@ -375,7 +418,6 @@ mod tests {
         let grid = solve(jets, 10);
         let top = part_two(&grid);
         assert_eq!(top, 17);
-
         let mut result: [u8; 60] = [
             0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
             0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
@@ -384,24 +426,24 @@ mod tests {
             0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
             0b0000000, // 19
             0b0000000, // 18
-            0b0000000, // 17
+            0b0000100, // 17
             0b0000100, // 16
-            0b0000100, // 15
-            0b0000110, // 14
-            0b1100110, // 13
-            0b1111110, // 12
-            0b0111000, // 11
-            0b0010000, // 10
-            0b0111100, // 09
+            0b0000110, // 15
+            0b1100110, // 14
+            0b1111110, // 13
+            0b0111000, // 12
+            0b0010000, // 11
+            0b0111100, // 10 
+            0b0000110, // 09
             0b0000110, // 08
-            0b0000110, // 07
-            0b0000100, // 06
+            0b0000100, // 07
+            0b0010100, // 06
             0b0010100, // 05
-            0b0010100, // 04
-            0b1111100, // 03
-            0b0011100, // 02
-            0b0001000, // 01
-            0b0011110, // 00
+            0b1111100, // 04
+            0b0011100, // 03
+            0b0001000, // 02
+            0b0011110, // 01
+            0b1111111, // 00
         ];
         result.reverse();
         assert_eq!(grid.grid, result);
@@ -470,6 +512,7 @@ mod tests {
         ];
         matrix.reverse();
         let mut grid = Grid::new(Some(matrix));
+        grid.high_index = 1_000_000;
         assert_eq!(grid.shift, 0);
         grid.shift();
 
@@ -499,19 +542,19 @@ mod tests {
 
     #[test]
     fn get_shapes_widths() {
-        let widths: Vec<isize> = get_shapes(7).iter().map(|shape| shape.width).collect();
+        let widths: Vec<usize> = get_shapes(7).iter().map(|shape| shape.width).collect();
         assert_eq!(widths, vec![4, 3, 3, 1, 2]);
     }
 
     #[test]
     fn get_shapes_height() {
-        let heights: Vec<isize> = get_shapes(7).iter().map(|shape| shape.height).collect();
+        let heights: Vec<usize> = get_shapes(7).iter().map(|shape| shape.height).collect();
         assert_eq!(heights, vec![1, 3, 3, 4, 2]);
     }
 
     #[test]
     fn get_shapes_right_border() {
-        let rb: Vec<isize> = get_shapes(7)
+        let rb: Vec<usize> = get_shapes(7)
             .iter()
             .map(|shape| shape.right_border)
             .collect();
@@ -529,6 +572,41 @@ mod tests {
     #[test]
     fn shape_move_right() {
         let mut shape = Shape::new(
+            4,
+            [
+                0b1111000, // ####...
+                0b0000000, // unused
+                0b0000000, // unused
+                0b0000000, // unused
+            ],
+        );
+        shape.set_right_border(7);
+        shape.set_location(0, 3);
+        assert_eq!(
+            shape.iter_rows().collect::<Vec<(usize, u8)>>(),
+            vec![(3, 0b1111000),]
+        );
+
+        shape.move_right();
+        assert_eq!(
+            shape.iter_rows().collect::<Vec<(usize, u8)>>(),
+            vec![(3, 0b0111100),]
+        );
+
+        shape.move_right();
+        shape.move_right();
+        assert_eq!(
+            shape.iter_rows().collect::<Vec<(usize, u8)>>(),
+            vec![(3, 0b0001111),]
+        );
+
+        shape.move_right();
+        assert_eq!(
+            shape.iter_rows().collect::<Vec<(usize, u8)>>(),
+            vec![(3, 0b0001111),]
+        );
+
+        let mut shape = Shape::new(
             3,
             [
                 0b0100000, // .#.
@@ -541,34 +619,19 @@ mod tests {
         shape.set_location(0, 9);
         assert_eq!(
             shape.iter_rows().collect::<Vec<(usize, u8)>>(),
-            vec![
-                (9, 0b0100000),
-                (8, 0b1110000),
-                (7, 0b0100000),
-                (6, 0b0000000),
-            ]
+            vec![(09, 0b0100000), (10, 0b1110000), (11, 0b0100000),]
         );
 
         shape.move_right();
         assert_eq!(
             shape.iter_rows().collect::<Vec<(usize, u8)>>(),
-            vec![
-                (9, 0b0010000),
-                (8, 0b0111000),
-                (7, 0b0010000),
-                (6, 0b0000000),
-            ]
+            vec![(09, 0b0010000), (10, 0b0111000), (11, 0b0010000),]
         );
 
         shape.move_right();
         assert_eq!(
             shape.iter_rows().collect::<Vec<(usize, u8)>>(),
-            vec![
-                (9, 0b0001000),
-                (8, 0b0011100),
-                (7, 0b0001000),
-                (6, 0b0000000),
-            ]
+            vec![(09, 0b0001000), (10, 0b0011100), (11, 0b0001000),]
         );
 
         shape.move_right();
@@ -576,36 +639,22 @@ mod tests {
         shape.move_right();
         assert_eq!(
             shape.iter_rows().collect::<Vec<(usize, u8)>>(),
-            vec![
-                (9, 0b0000010),
-                (8, 0b0000111),
-                (7, 0b0000010),
-                (6, 0b0000000),
-            ]
+            vec![(09, 0b0000010), (10, 0b0000111), (11, 0b0000010),]
         );
     }
+
     #[test]
     fn shape_apply_location() {
         let mut shape = Shape::new(4, [0b1111000, 0b0000000, 0b0000000, 0b0000000]);
         shape.set_location(1, 0);
         assert_eq!(
             shape.iter_rows().collect::<Vec<(usize, u8)>>(),
-            vec![
-                (0, 0b0111100),
-                // (1, 0b0000000),
-                // (2, 0b0000000),
-                // (3, 0b0000000),
-            ]
+            vec![(0, 0b0111100),]
         );
         shape.set_location(3, 8);
         assert_eq!(
             shape.iter_rows().collect::<Vec<(usize, u8)>>(),
-            vec![
-                (8, 0b0001111),
-                (7, 0b0000000),
-                (6, 0b0000000),
-                (5, 0b0000000),
-            ]
+            vec![(8, 0b0001111),]
         );
 
         let mut shape = Shape::new(
@@ -620,12 +669,7 @@ mod tests {
         shape.set_location(2, 5);
         assert_eq!(
             shape.iter_rows().collect::<Vec<(usize, u8)>>(),
-            vec![
-                (5, 0b0001000),
-                (4, 0b0011100),
-                (3, 0b0001000),
-                (2, 0b0000000),
-            ]
+            vec![(5, 0b0001000), (6, 0b0011100), (7, 0b0001000),]
         );
     }
 
@@ -665,7 +709,7 @@ mod tests {
                 0b0000000, // unused
             ],
         );
-        shape.set_location(0, 2);
+        shape.set_location(4, 1);
         grid.draw_shape(&shape);
 
         let mut result: [u8; 60] = [
@@ -682,10 +726,10 @@ mod tests {
             0b0001111, // 6
             0b0000000, // 5
             0b0000000, // 4
-            0b0111100, // 3
-            0b0100000, // 2
-            0b1110000, // 1
-            0b0100000, // 0
+            0b0111110, // 3
+            0b0000111, // 2
+            0b0000010, // 1
+            0b0000000, // 0
         ];
         result.reverse();
         assert_eq!(grid.grid, result);
@@ -747,7 +791,7 @@ mod tests {
                 0b0000000, // unused
             ],
         );
-        shape.set_location(4, 6);
+        shape.set_location(4, 5);
         assert_eq!(grid.contains(&shape), false);
 
         shape.set_location(2, 7);
@@ -757,7 +801,7 @@ mod tests {
     #[test]
     fn grid_draw_top() {
         let mut grid = Grid::new(Some([
-            0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
+            0b1111111, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
             0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
             0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
             0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
@@ -766,7 +810,7 @@ mod tests {
             0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000, 0b0000000,
             0b0000000, 0b0000000, 0b0000000, 0b0000000,
         ]));
-        assert_eq!(grid.high_index, -1);
+        assert_eq!(grid.high_index, 0);
 
         let mut shape = Shape::new(
             3,
@@ -780,10 +824,10 @@ mod tests {
 
         shape.set_location(0, 15);
         grid.draw_shape(&shape);
-        assert_eq!(grid.high_index, 15);
+        assert_eq!(grid.high_index, 17);
 
         shape.set_location(0, 5);
         grid.draw_shape(&shape);
-        assert_eq!(grid.high_index, 15);
+        assert_eq!(grid.high_index, 17);
     }
 }
