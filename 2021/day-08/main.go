b8b3a2f8aa1472e7acfe186ae3e6132098b2ec45
Roman Timashev <roman@tmshv.ru>
2023-07-29 04:17:58 +0300

    solve day 8

--- 2021/day-08/main.go
+++ 2021/day-08/main.go
@@ -29,7 +29,7 @@ import (
 // .    f  e    f  .    f  e    f  .    f
 //  gggg    gggg    ....    gggg    gggg
 
-func getReferenceDigits() []int8 {
+func getReferenceDigits() map[int8]int {
 	const (
 		A = 0
 		B = 1
@@ -39,36 +39,17 @@ func getReferenceDigits() []int8 {
 		F = 5
 		G = 6
 	)
-	return []int8{
-		// 0
-		1<<A | 1<<B | 1<<C | 1<<E | 1<<F | 1<<G,
-
-		// 1
-		1<<C | 1<<F,
-
-		// 2
-		1<<A | 1<<C | 1<<D | 1<<E | 1<<G,
-
-		// 3
-		1<<A | 1<<C | 1<<D | 1<<F | 1<<G,
-
-		// 4
-		1<<B | 1<<C | 1<<D | 1<<F,
-
-		// 5
-		1<<A | 1<<B | 1<<D | 1<<F | 1<<G,
-
-		// 6
-		1<<A | 1<<B | 1<<D | 1<<E | 1<<F | 1<<G,
-
-		// 7
-		1<<A | 1<<C | 1<<F,
-
-		// 8
-		1<<A | 1<<B | 1<<C | 1<<D | 1<<E | 1<<F | 1<<G,
-
-		// 9
-		1<<A | 1<<B | 1<<C | 1<<D | 1<<F | 1<<G,
+	return map[int8]int{
+		1<<A | 1<<B | 1<<C | 1<<E | 1<<F | 1<<G:        0,
+		1<<C | 1<<F:                                    1,
+		1<<A | 1<<C | 1<<D | 1<<E | 1<<G:               2,
+		1<<A | 1<<C | 1<<D | 1<<F | 1<<G:               3,
+		1<<B | 1<<C | 1<<D | 1<<F:                      4,
+		1<<A | 1<<B | 1<<D | 1<<F | 1<<G:               5,
+		1<<A | 1<<B | 1<<D | 1<<E | 1<<F | 1<<G:        6,
+		1<<A | 1<<C | 1<<F:                             7,
+		1<<A | 1<<B | 1<<C | 1<<D | 1<<E | 1<<F | 1<<G: 8,
+		1<<A | 1<<B | 1<<C | 1<<D | 1<<F | 1<<G:        9,
 	}
 }
 
@@ -77,7 +58,7 @@ type Sample struct {
 	digits []int8
 }
 
-func (s *Sample) decode(shifts []int8, ref map[int8]bool) bool {
+func (s *Sample) Check(shifts []int8, ref map[int8]int) bool {
 	decoded := make([]int8, len(s.noise))
 	for i, n := range s.noise {
 		powers := int8ToPowers(n)
@@ -90,6 +71,18 @@ func (s *Sample) decode(shifts []int8, ref map[int8]bool) bool {
 	return true
 }
 
+func (s *Sample) Decode(shifts []int8, ref map[int8]int) int {
+	result := 0
+	for i, d := range s.digits {
+		powers := int8ToPowers(d)
+		digit := getDigit(powers, shifts)
+        val := ref[digit]
+        decimalPower := len(s.digits) - i - 1
+		result += val * pow(10, decimalPower)
+	}
+	return result
+}
+
 func getDigit(xs, shifts []int8) int8 {
 	var digit int8
 	for _, x := range xs {
@@ -97,6 +90,17 @@ func getDigit(xs, shifts []int8) int8 {
 		digit |= 1 << shift
 	}
 	return digit
+}
+
+func pow(x, y int) int {
+	if y == 0 {
+		return 1
+	}
+	val := x // x ^ 1
+	for i := 2; i <= y; i++ {
+		val = val * x
+	}
+	return val
 }
 
 func permutations(arr []int8) [][]int8 {
@@ -178,39 +182,45 @@ func readInput() ([]Sample, error) {
 }
 
 func solvePartOne(samples []Sample) int {
-	reference := getReferenceDigits()
-	ref := map[int8]bool{}
-	for _, i := range reference {
-		ref[i] = true
+	ref := getReferenceDigits()
+	partOne := map[int]bool{
+		1: true,
+		4: true,
+		7: true,
+		8: true,
 	}
-
-	partOne := map[int8]bool{}
-	partOne[reference[1]] = true
-	partOne[reference[4]] = true
-	partOne[reference[7]] = true
-	partOne[reference[8]] = true
-
 	count := 0
 	for _, sample := range samples {
 		for _, shifts := range permutations([]int8{0, 1, 2, 3, 4, 5, 6}) {
-			if !sample.decode(shifts, ref) {
+			if !sample.Check(shifts, ref) {
 				continue
 			}
 			for _, d := range sample.digits {
 				powers := int8ToPowers(d)
 				digit := getDigit(powers, shifts)
-				if _, ok := partOne[digit]; ok {
+				val := ref[digit]
+				if _, ok := partOne[val]; ok {
 					count += 1
 				}
 			}
 		}
 	}
-
 	return count
 }
 
 func solvePartTwo(samples []Sample) int {
-	return 0
+	reference := getReferenceDigits()
+	count := 0
+	for _, sample := range samples {
+		for _, shifts := range permutations([]int8{0, 1, 2, 3, 4, 5, 6}) {
+			if !sample.Check(shifts, reference) {
+				continue
+			}
+			count += sample.Decode(shifts, reference)
+		}
+	}
+
+	return count
 }
 
 func main() {
