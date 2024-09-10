ddf96da9712551ed35285e18a8128aa96b7a9491
Roman Timashev <roman@tmshv.ru>
2024-09-11 02:35:16 +0300

    impl part two

--- 2023/day01/main.c
+++ 2023/day01/main.c
@@ -58,6 +58,107 @@ int partOne() {
 	return result;
 }
 
+char convertTextToDigit(char *text) {
+	if (strcmp(text, "one") == 0) {
+		return '1';
+	} else if (strcmp(text, "two") == 0) {
+		return '2';
+	} else if (strcmp(text, "three") == 0) {
+		return '3';
+	} else if (strcmp(text, "four") == 0) {
+		return '4';
+	} else if (strcmp(text, "five") == 0) {
+		return '5';
+	} else if (strcmp(text, "six") == 0) {
+		return '6';
+	} else if (strcmp(text, "seven") == 0) {
+		return '7';
+	} else if (strcmp(text, "eight") == 0) {
+		return '8';
+	} else if (strcmp(text, "nine") == 0) {
+		return '9';
+	}
+	return '0';
+}
+
+// kjxone -> k[jxone] -> jxone -> no
+// kjxone -> kj[xone] -> xone  -> no
+// kjxone -> kjx[one] -> one   -> bingo!
+char convertSuffixTextToDigit(char *text, int *offset) {
+	char buffer[MAX_LINE_LENGTH];
+	int len = strlen(text);
+	for (int i = 0; i < len; i++) {
+		int size = len - i;
+		strncpy(buffer, text + i, size);
+		buffer[size] = '\0';
+
+		char digit = convertTextToDigit(buffer);
+		if (digit != '0') {
+			*offset = i;
+			return digit;
+		}
+	}
+
+	return '0';
+}
+
+int partTwo() {
+	int records;
+	char **input = readInput(&records);
+
+	// Patch input for part two
+	// 1. Replace first leter with digit
+	// 2. one -> 1ne
+	// 3. twone -> 2w1ne
+	// 4. jxone6 -> jx1ne6
+	//
+	// Example of patch:
+	// 7pqrstsixteentwone
+	// 7pqrst6ixteen2w1ne
+	char buffer[MAX_LINE_LENGTH];
+	for (int i = 0; i < records; i++) {
+		char *line = input[i];
+
+		int digitStartIndex = 0;
+		int offset = 0;
+		for (int j = 0; j < strlen(line); j++) {
+			if (line[j] >= '1' && line[j] <= '9') {
+				digitStartIndex = j + 1;
+				continue;
+			}
+
+			int size = j - digitStartIndex + 1;
+			strncpy(buffer, line + digitStartIndex, size);
+			buffer[size] = '\0';
+
+			char digit = convertSuffixTextToDigit(buffer, &offset);
+			if (digit != '0') {
+				digitStartIndex += offset;
+				line[digitStartIndex] = digit;
+
+				// Move next iteration to the next index after replaced
+				j = digitStartIndex + 1;
+			}
+		}
+
+		// Check last buffer after iteration stops
+		char digit = convertSuffixTextToDigit(buffer, &digitStartIndex);
+		if (digit != '0') {
+			digitStartIndex += offset;
+			line[digitStartIndex] = digit;
+		}
+	}
+
+	int result = solve(input, records);
+
+	for (int i = 0; i < records; i++) {
+		free(input[i]);
+	}
+	free(input);
+
+	return result;
+}
+
 int getPart(char **argv) {
 	if (strcmp(argv[1], "one") == 0) {
 		return 1;
@@ -82,7 +183,8 @@ int main(int argc, char **argv) {
 		printf("Part one: %d\n", result);
 		break;
 	case 2:
-		printf("Part two is not implemented yet.\n");
+		result = partTwo();
+		printf("Part two: %d\n", result);
 		break;
 	default:
 		fprintf(stderr, "Invalid argument: %s\n", argv[1]);
