c590645ddd6eaaeb8321f3d8ce4f810cb62f53c3
Roman Timashev <roman@tmshv.ru>
2024-09-11 00:54:38 +0300

    refactor part one

--- 2023/day01/main.c
+++ 2023/day01/main.c
@@ -2,50 +2,92 @@
 #include <stdlib.h>
 #include <string.h>
 
-int partOne() {
-	int result = 0;
-	// Buffer to hold each line read from stdin
-	char line[256];
+#define MAX_LINES 10000
+#define MAX_LINE_LENGTH 256
 
-	// Read lines from stdin until EOF is encountered
+char **readInput(int *lineCount) {
+	char **lines = (char **)malloc(MAX_LINES * sizeof(char *));
+	char line[MAX_LINE_LENGTH];
+	*lineCount = 0;
+
 	while (fgets(line, sizeof(line), stdin) != NULL) {
-		char first;
-		char last;
+		lines[*lineCount] = (char *)malloc((strlen(line) + 1) * sizeof(char));
+		strcpy(lines[*lineCount], line);
+		(*lineCount)++;
+	}
 
-		char c;
-		unsigned long len = strlen(line);
-		for (int i = 0; i < len; i++) {
-			c = line[i];
-			if (c >= '1' && c <= '9') {
-				first = c;
+	return lines;
+}
+
+int solve(char **input, int records) {
+	int result = 0;
+	for (int i = 0; i < records; i++) {
+		char first, last;
+		int len = strlen(input[i]);
+		for (int j = 0; j < len; j++) {
+			if (input[i][j] >= '1' && input[i][j] <= '9') {
+				first = input[i][j];
 				break;
 			}
 		}
 
-		for (int i = len; i >= 0; i--) {
-			c = line[i];
-			if (c >= '1' && c <= '9') {
-				last = c;
+		for (int j = len - 1; j >= 0; j--) {
+			if (input[i][j] >= '1' && input[i][j] <= '9') {
+				last = input[i][j];
 				break;
 			}
 		}
 
 		char str[3] = {first, last, '\0'};
 		int calibration = atoi(str);
-
-		printf("%s -> %d\n", line, calibration);
-
 		result += calibration;
 	}
+	return result;
+}
+
+int partOne() {
+	int records;
+	char **input = readInput(&records);
+	int result = solve(input, records);
+
+	for (int i = 0; i < records; i++) {
+		free(input[i]);
+	}
+	free(input);
 
 	return result;
 }
 
-int main() {
-	int result;
+int getPart(char **argv) {
+	if (strcmp(argv[1], "one") == 0) {
+		return 1;
+	} else if (strcmp(argv[1], "two") == 0) {
+		return 2;
+	} else {
+		return -1;
+	}
+}
 
-	result = partOne();
-	printf("Part one: %d\n", result);
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <one|two>\n", argv[0]);
+		return 1;
+	}
+
+	int result;
+	int part = getPart(argv);
+	switch (part) {
+	case 1:
+		result = partOne();
+		printf("Part one: %d\n", result);
+		break;
+	case 2:
+		printf("Part two is not implemented yet.\n");
+		break;
+	default:
+		fprintf(stderr, "Invalid argument: %s\n", argv[1]);
+		return 1;
+	}
 
 	return 0;
 }
